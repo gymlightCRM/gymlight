62ae36ab060085ad3fe76fcc4ce970ff77fa1c36
parkyongsung13 <129723422+parkyongsung13@users.noreply.github.com>
2023-04-18 18:24:28 +0900

    super

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get("/", function(req,res){
     res.send("hello world");
 })
 
+//회사소개 페이지
+app.get("/about", function(req,res){
+    res.send('회사홈페이지');
+})
+
+
 app.listen(3000,function(req,res){
     console.log("서버가 실행되고 있다")
 })
+
+
